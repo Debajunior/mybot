da5451c2ec29343510599918531c02f270a10048
Debanjan Ghosh <imdeghosh@gmail.com>
2019-09-10 20:26:19 -0400

    Add files via upload

--- botConfig.py
+++ botConfig.py
@@ -1,10 +1,10 @@
 #! /usr/bin/python3
 
 ##Change the following values to customize your ChatBot:
-myBotName = "Marvin the ChatBot"
+myBotName = "Debajunior"
 botTimeZone = "America/New_York"  ##See the list full below
-botAvatar = "/static/bot.png" ##This can either be an image in your static folder or a web url to an image
-chatBG = "/static/bg.jpg"  ##This can either be an image in your static folder or a web url to an image
+botAvatar = "/static/Prime.jpg" ##This can either be an image in your static folder or a web url to an image
+chatBG = "/static/OptimusPrime.png"  ##This can either be an image in your static folder or a web url to an image
 useGoogle = "yes" ## yes or no if you want Google links provided when your Bot does not have an answer
 confidenceLevel = 0.65 ##Bot confidence level - must be betwee. 0.00 and 1, default is 0.65
 
